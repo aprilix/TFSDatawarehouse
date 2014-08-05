1acd79514e7856eabfe404aa70c6f880621366b1
Gerson Dias <gerson.dias@lambda3.com.br>
2014-08-05 15:36:15 -0300

    Inclusao do projeto de setup e reorganização do projeto

---
+++ SetupWarehouseAdapter/ManageCustomWarehouseJob.cs
@@ -0,0 +1,129 @@
+﻿using SetupWarehouseAdapter.Properties;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupWarehouseAdapter
+{
+    /// <summary>
+    /// A simple command line tool used to create or delete a Warehouse job and schedule for the CSharp Assembly Code Churn adapter.
+    /// 
+    /// In order to fully install and configure this custom adapter you need to do the following:
+    ///  - Build the CSharpAssemblyCodeChurnSample.Adapter and copy it to the 
+    ///      %ProgramFiles%\Microsoft Team Foundation Server 2010\Application Tier\TFSJobAgent\plugins
+    ///    directory on all of your Team Foundation Server Application Tier machines.  Assemblies in
+    ///    this location will be loaded by the TFS Job Agent.
+    ///  - Restart the "Visual Studio Team Foundation Background Job Agent" service.  This will
+    ///    force the TFS Job Agent to reload all the job plugins.
+    ///      net stop TfsJobAgent
+    ///      net start TfsJobAgent
+    ///  - Run this executable with the -c flag.  This will create a TFS Job and schedule for the custom
+    ///    adapter.
+    /// 
+    /// In order to fully uninstall this custom adapter and remove all related schema from the Warehouse 
+    /// you will need to do the following:
+    ///  - Run this executable with the -d flag.  This will delete the TFS Job and schedule for the custom
+    ///    adapter.
+    ///  - Delete the CSharpAssemblyCodeChurnSample.Adapter assembly from the 
+    ///      %ProgramFiles%\Microsoft Team Foundation Server 2010\Application Tier\TFSJobAgent\plugins
+    ///    directory.
+    ///  - Rebuild the Warehouse
+    ///      Either use TfsConfig.exe rebuildWarehouse /all
+    ///              or Tfs Administration Console, under the Application Tier -> Reporting pane click "Start Rebuild"
+    ///    This will rebuild the Warehouse schema and data without the custom schema added by this adapter.
+    /// </summary>
+    public class ManageCustomWarehouseJob
+    {
+        static void Main(string[] args)
+        {
+            var jobName = "";
+            var syncJobExtension = "";
+            var collection = "";
+            var create = false;
+            var delete = false;
+            var minutesInterval = 2;
+
+            if (args.GetLength(0) == 0)
+            {
+                Console.WriteLine(Resources.Usage);
+                return;
+            }
+
+            foreach (var arg in args)
+            {
+                var nomeArgumento = arg.ToLower();
+
+                if (nomeArgumento.Contains("="))
+                    nomeArgumento = nomeArgumento.Split('=')[0];
+
+                if (nomeArgumento == "-jobname")
+                {
+                    jobName = arg.Split('=')[1];
+                }
+                else if (nomeArgumento == "-syncjobextension")
+                {
+                    syncJobExtension = arg.Split('=')[1];
+                }
+                else if (nomeArgumento == "-collection")
+                {
+                    collection = arg.Split('=')[1];
+                }
+                else if (nomeArgumento == "-c")
+                {
+                    create = true;
+                }
+                else if (nomeArgumento == "-d")
+                {
+                    delete = true;
+                }
+                else if (nomeArgumento == "-t")
+                {
+                    minutesInterval = int.Parse(arg.Split('=')[1]);
+                }
+                else
+                {
+                    Console.WriteLine(Resources.Usage);
+                    return;
+                }
+            }
+
+            var jobManager = new WarehouseJobManager(new Uri(collection));
+
+            if (create)
+            {
+                if (jobManager.CreateJob(jobName, syncJobExtension, minutesInterval))
+                {
+                    Console.WriteLine(String.Format(CultureInfo.CurrentCulture,
+                        Resources.CreatedJob,
+                        jobName, syncJobExtension));
+                }
+                else
+                {
+                    Console.WriteLine(String.Format(CultureInfo.CurrentCulture,
+                        Resources.JobAlreadyExists, jobName));
+                }
+            }
+            else if (delete)
+            {
+                if (jobManager.DeleteJob(jobName))
+                {
+                    Console.WriteLine(String.Format(CultureInfo.CurrentCulture,
+                        Resources.JobDeleted, jobName));
+                }
+                else
+                {
+                    Console.WriteLine(String.Format(CultureInfo.CurrentCulture,
+                        Resources.JobNotFound, jobName));
+                }
+            }
+            else
+            {
+                Console.WriteLine(Resources.Usage);
+                return;
+            }
+        }
+    }
+}
---
+++ SetupWarehouseAdapter/WarehouseJobManager.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SetupWarehouseAdapter
+{
+    internal class WarehouseJobManager
+    {
+        /// <summary>
+        /// The data element name used in the job definition.
+        /// </summary>
+        private const string JobDataElementName = "Data";
+
+        /// <summary>
+        /// The data element value used in the job definition identifying this as a Warehouse job.
+        /// </summary>
+        private const string WarehouseJobDataElementValue = "JobCategories=Warehouse;";
+
+        /// <summary>
+        /// The default processing interval is 2 minutes in TFS 2010.  If you would like to have a different run interval
+        /// you must use the WarehouseControlWebService ChangeSetting method.  The value set here will be overwritten
+        /// the next time the Warehouse is brought offline then back online.
+        /// </summary>
+        private int WarehouseJobDefaultProcessingInterval = 60; // 2 minutes.
+
+        private TfsTeamProjectCollection TeamProjectCollection { get; set; }
+        private ITeamFoundationJobService CollectionJobService { get; set; }
+
+        /// <summary>
+        /// Create a new Warehouse job manager wrapper connected to the Team Project Collection with the given
+        /// uri.
+        /// </summary>
+        /// <param name="tfsCollectionUri">Uri to the team project collection (i.e. http://localhost:8080/tfs/Collection0)</param>
+        public WarehouseJobManager(Uri tfsCollectionUri)
+        {
+            TeamProjectCollection = new TfsTeamProjectCollection(tfsCollectionUri);
+            CollectionJobService = TeamProjectCollection.GetService<ITeamFoundationJobService>();
+        }
+
+        /// <summary>
+        /// Create the custom Warehouse job if one with the same name doesn't already exist.
+        /// </summary>
+        /// <param name="jobName">Job name.</param>
+        /// <param name="jobExtensionName">Job extension.</param>
+        public bool CreateJob(string jobName, string jobExtensionName, int minutesInterval = 2)
+        {
+            TeamFoundationJobDefinition warehouseJob;
+            if (TryGetJobByName(jobName, out warehouseJob))
+            {
+                return false;
+            }
+
+            WarehouseJobDefaultProcessingInterval *= minutesInterval;
+
+            warehouseJob = CreateWarehouseJobDefinition(jobName, jobExtensionName);
+            CollectionJobService.UpdateJob(warehouseJob);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Delete the job with the specified name.
+        /// </summary>
+        /// <param name="jobName">Name of job.</param>
+        /// <returns>True if a job was deleted, false otherwise.</returns>
+        public bool DeleteJob(string jobName)
+        {
+            TeamFoundationJobDefinition warehouseJob;
+            if (TryGetJobByName(jobName, out warehouseJob))
+            {
+                CollectionJobService.DeleteJob(warehouseJob);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Try to find a job definition with a specified name
+        /// </summary>
+        /// <param name="jobName">Job name.</param>
+        /// <param name="jobDefinition">Job definition found, NULL if none found.</param>
+        /// <returns>True if a job was found with the name jobName otherwise False.</returns>
+        private bool TryGetJobByName(string jobName, out TeamFoundationJobDefinition jobDefinition)
+        {
+            jobDefinition = null;
+
+            var jobDefinitions = CollectionJobService.QueryJobs();
+
+            foreach (var definition in jobDefinitions)
+            {
+                if (String.Equals(definition.Name, jobName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    jobDefinition = definition;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Create a new job definition for a Warehouse job.
+        /// </summary>
+        /// <remarks>
+        /// This set up the job with the properties required for it to be considered a Warehouse job.
+        /// 
+        /// TeamFoundationJobDefinition is sealed so you cannot derive a strongly typed warehouse job.
+        /// </remarks>
+        /// <param name="jobName">Job name.</param>
+        /// <param name="jobExtensionName">Extension name (assembly.class)</param>
+        /// <returns>New job definition for the Warehouse job.</returns>
+        private TeamFoundationJobDefinition CreateWarehouseJobDefinition(string jobName, string jobExtensionName)
+        {
+            // Add data node identifying this as a Warehouse job.
+            var document = new XmlDocument();
+            var dataElement = document.CreateElement(JobDataElementName);
+            dataElement.InnerText = WarehouseJobDataElementValue;
+
+            // Create a job definition
+            var newDefinition = new TeamFoundationJobDefinition(jobName, jobExtensionName, dataElement);
+
+            // Create a default schedule for the job.  
+            var schedule = new TeamFoundationJobSchedule(DateTime.UtcNow, WarehouseJobDefaultProcessingInterval);
+            newDefinition.Schedule.Add(schedule);
+
+            return newDefinition;
+        }
+    }
+}
